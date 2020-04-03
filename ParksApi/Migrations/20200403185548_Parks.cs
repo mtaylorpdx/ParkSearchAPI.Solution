4b5f11005660955e83d7151fd118d73e66504576
mtaylorpdx <taylor.matt@protonmail.com>
2020-04-03 11:56:47 -0700

    new migrations

---
+++ ParksApi/Migrations/20200403185548_Parks.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksApi.Migrations
+{
+    public partial class Parks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StateParks_NationalParks_NationalParkId",
+                table: "StateParks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StateParks_NationalParkId",
+                table: "StateParks");
+
+            migrationBuilder.DropColumn(
+                name: "NationalParkId",
+                table: "StateParks");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NationalParkId",
+                table: "StateParks",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StateParks_NationalParkId",
+                table: "StateParks",
+                column: "NationalParkId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StateParks_NationalParks_NationalParkId",
+                table: "StateParks",
+                column: "NationalParkId",
+                principalTable: "NationalParks",
+                principalColumn: "NationalParkId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
