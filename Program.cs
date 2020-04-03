d6437f56864f9891aaa503cc7d602b61820d925a
mtaylorpdx <taylor.matt@protonmail.com>
2020-04-03 09:47:06 -0700

    fix typo

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace ParksAPI.Solution
+namespace ParksApi.Solution
 {
     public class Program
     {
